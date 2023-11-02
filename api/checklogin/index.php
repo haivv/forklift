ad2eb07af9af21cbc4a7fb9a4d7417a403e46d1b
Van-Hai Vu <50228260+haivv@users.noreply.github.com>
2023-11-02 17:25:23 +0900

    Update index.php

--- api/checklogin/index.php
+++ api/checklogin/index.php
@@ -5,6 +5,7 @@ mysqli_set_charset( $conn, 'utf8' );
 $username = $_POST[ 'username' ];
 $password = $_POST[ 'password' ];
 
+
 $result = mysqli_query( $conn, "SELECT * FROM  account WHERE username='$username'" );
 $num = mysqli_num_rows( $result );
 if ( $num > 0 ) {
@@ -27,6 +28,27 @@ if ( $num > 0 ) {
         } else {
             echo 'user exist, login fail';
         }
+        
+            $dataToSave2 = [
+                'idAcc' => "",
+                'username' => "",
+                'password' => "",
+                'name' => "",
+                'job' =>""
+            ];
+
+            $jsonString2 = json_encode( $dataToSave2, JSON_PRETTY_PRINT );
+            // You can use JSON_PRETTY_PRINT for pretty formatting
+    
+            // Specify the path to the JSON file
+            $jsonFilePath2 = 'oneinfor.json';
+            // Save the JSON string to the file
+            if ( file_put_contents( $jsonFilePath2, $jsonString2 ) ) {
+                echo 'save to one info success';
+            }
+
+
+       
 
     } else {
         $dataToSave = [
@@ -45,6 +67,27 @@ if ( $num > 0 ) {
         } else {
             echo 'user exist, login success';
         }
+
+        while ($onerow = mysqli_fetch_assoc($result2)) {
+            $dataToSave2 = [
+                'idAcc' => $onerow["idAcc"],
+                'username' => $onerow["username"],
+                'password' => $onerow["password"],
+                'name' => $onerow["name"],
+                'job' => $onerow["job"]
+            ];
+
+            $jsonString2 = json_encode( $dataToSave2, JSON_PRETTY_PRINT );
+            // You can use JSON_PRETTY_PRINT for pretty formatting
+    
+            // Specify the path to the JSON file
+            $jsonFilePath2 = 'oneinfor.json';
+            // Save the JSON string to the file
+            if ( file_put_contents( $jsonFilePath2, $jsonString2 ) ) {
+                echo 'save to one info success';
+            }
+        }
+
     }
 } else {
     $dataToSave = [
@@ -60,12 +103,29 @@ if ( $num > 0 ) {
     // Save the JSON string to the file
     if ( file_put_contents( $jsonFilePath, $jsonString ) ) {
         echo 'user: not exist';
-    } else {
-        echo 'user not exist';
     }
+
+    
+        $dataToSave2 = [
+            'idAcc' => "",
+            'username' => "",
+            'password' => "",
+            'name' => "",
+            'job' =>""
+        ];
+
+        $jsonString2 = json_encode( $dataToSave2, JSON_PRETTY_PRINT );
+        // You can use JSON_PRETTY_PRINT for pretty formatting
+
+        // Specify the path to the JSON file
+        $jsonFilePath2 = 'oneinfor.json';
+        // Save the JSON string to the file
+        if ( file_put_contents( $jsonFilePath2, $jsonString2 ) ) {
+            echo 'save to one info success';
+        }
+
 }
 
 mysqli_close( $conn );
 
 ?>
-
